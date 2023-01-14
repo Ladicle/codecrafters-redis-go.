e43eb2bf3c10f7759006a4db1be34f1aa6d507b6
Aya Igarashi <ladiclexxx@gmail.com>
2023-01-14 13:55:26 +0900

    Say pong multiple times

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"os"
@@ -9,27 +10,43 @@ import (
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
+	if err := run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
 
+func run() error {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		return fmt.Errorf("Failed to bind to port 6379")
 	}
 
-	var buf []byte
-	if _, err := conn.Read(buf); err != nil {
-		fmt.Println("Error read data:", err.Error())
-		os.Exit(1)
-	}
-	fmt.Println("read:", string(buf))
-
-	if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
-		fmt.Println("Error write data:", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			return fmt.Errorf("Error accepting connection: %s", err)
+		}
+		go func(c net.Conn) {
+			defer conn.Close()
+			for {
+				buf := make([]byte, 4*1024)
+				if _, err := conn.Read(buf); err != nil {
+					fmt.Println(err)
+					return
+				}
+				fmt.Printf("%s\n", buf)
+				if bytes.Contains(buf, []byte("PING")) || bytes.Contains(buf, []byte("ping")) {
+					fmt.Println("write: PONG")
+					if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
+						return
+					}
+				} else {
+					if _, err := conn.Write([]byte("+OK\r\n")); err != nil {
+						return
+					}
+				}
+			}
+		}(conn)
 	}
 }
